3e1db8be01511a94665ab78ed786f6745a6ce3a5
Swetha <swetha17aug@gmail.com>
2024-05-08 16:06:28 +0530

    Remove the duplicate character
    This Python script demonstrates how to remove duplicate characters from a given string and store the unique characters in a variable. It iterates over each character in the string and checks if it is already present in the Var_name variable. If not, it appends the character to Var_name. Finally, it converts the original string into a list of characters using the list() function and stores it in Var_name1.

---
+++ Task 3.py
@@ -0,0 +1,11 @@
+String = "Iam become a python developer" #Given a string
+Var_name = ""
+for i in String:
+    if i not in Var_name: #Using Operator
+        Var_name = Var_name+i
+print(Var_name)
+Var_name1 = list("Iam become a python developer")
+
+
+
+
